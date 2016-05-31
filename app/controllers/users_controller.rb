5736772ab72f2be4a658231a07b81634360a3848
relative-rene <rene.arellano415@gmail.com>
2016-05-31 11:45:52 -0700

    listing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user= User.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:user].inspect
+    @user = User.new(params.require(:user).permit(:first_name, :last_name, :password_digest, :phone_number, :email)
+
+    @user.save
+    redirect_to @user
   end
 end
