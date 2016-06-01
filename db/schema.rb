c075ebddbdc6b4c2dfc7a34538f98fdbc9948fde
relative-rene <rene.arellano415@gmail.com>
2016-06-01 12:43:50 -0700

    navbar splash page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531225757) do
+ActiveRecord::Schema.define(version: 20160601014152) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "user_id"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20160531225757) do
   end
 
   add_index "appointments", ["user_id"], name: "index_appointments_on_user_id"
+
+  create_table "time_slots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
