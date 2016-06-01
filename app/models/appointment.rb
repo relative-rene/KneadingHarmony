acf62e16bcc6458abf7f823a560ad6ef3fa6d3e2
relative-rene <rene.arellano415@gmail.com>
2016-05-31 18:55:22 -0700

    timeslot, appointment and user

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ActiveRecord::Base
   belongs_to :user
-  
+  has_many :timeslots
+
+  def valid?
+    taken = where(day: day, time_slot_id: time_slot_id)
+    save unless taken
+  end
 end
