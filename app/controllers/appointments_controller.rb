5549b7b7e9463a679ae10d599de7d81968eb474e
relative-rene <rene.arellano415@gmail.com>
2016-05-31 18:55:22 -0700

    timeslot, appointment and user

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,7 @@
 class AppointmentsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
-    @appointment = @user.appointments.create(appointment_params)
+    @appointment = @user.appointments.create(appointment_params).valid?
     redirect_to user_path(@user)
   end
 
@@ -15,7 +15,7 @@ class AppointmentsController < ApplicationController
 
   private
     def appointment_params
-      params.require(:appointment).permit(:service, :date)
+      params.require(:appointment).permit(:time_slot_id, :day).merge(user_id: current_user.id)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :appointments, dependent: :destroy
   validates :email, :password_digest, :phone_number, :credit_card, presence: true, uniqueness:true
+
+  def valid?
+    taken = where("start <= ? AND end >= ?", start, end)
+    save unless taken
+  end
 end
