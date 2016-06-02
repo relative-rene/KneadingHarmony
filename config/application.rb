9df1d0d8b2e4706f48c63d14b580ae924b19c1a2
relative-rene <rene.arellano415@gmail.com>
2016-06-02 12:28:39 -0700

    calendar routes set up

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-module CalendarExample
+module AppointmentProject03
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
+        config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -20,11 +20,6 @@ module CalendarExample
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  end
-end
-
-module AppointmentProject03
-  class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
