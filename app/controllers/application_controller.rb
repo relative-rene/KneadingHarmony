d9b102a82f9c99fb158e12ae432d86d2e51175e1
relative-rene <rene.arellano415@gmail.com>
2016-06-02 15:46:48 -0700

    timezone cookie for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_time_zone, if: :logged_in?
-
+  before_filter :set_timezone
+  
   include SessionsHelper
   include CalendarHelper
 
   private
 
-    def set_time_zone
-      Time.zone = current_user.time_zone
+    def set_timezone
+     min = request.cookies["time_zone"].to_i
+     Time.zone = ActiveSupport::TimeZone[-min.minutes]
     end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602212309) do
+ActiveRecord::Schema.define(version: 20160602221730) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "user_id"
