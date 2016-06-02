9de30a9622f560893e9ccc48bb6bea36c7e39879
relative-rene <rene.arellano415@gmail.com>
2016-06-02 15:00:00 -0700

    log in routed session helper

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,16 @@ Rails.application.routes.draw do
   resources :users do
     resources :appointments
   end
-
   root to: 'welcome#index'
   #About route
   get "/about", to: "about#show", as: "about"
-
   #Policy route
   get "/policy", to: "policy#show", as: "policy"
-
   #Services
   get "/services", to: "services#index", as: "services"
+  #Session routes
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create", as: "post_session"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601024640) do
+ActiveRecord::Schema.define(version: 20160602212309) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "user_id"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160601024640) do
   end
 
   add_index "appointments", ["user_id"], name: "index_appointments_on_user_id"
+
+  create_table "time_slots", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
