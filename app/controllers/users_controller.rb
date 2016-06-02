1b06c6b71345942291fd829d65686c45a3fc6941
relative-rene <rene.arellano415@gmail.com>
2016-06-02 16:07:43 -0700

    sign up linked debugged

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,14 +63,16 @@ end
     end
   end
 
-private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    params.require(:user).permit(:name, :email, :login)
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :login)
+    end
   end
 end
