2f2d30927a8ce83d90a0984e7feb1b8ba4ef08a1
relative-rene <rene.arellano415@gmail.com>
2016-06-03 05:41:49 -0700

    timeslots controller

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -15,10 +15,15 @@ class TimeSlotsController < ApplicationController
   # GET /time_slots/new
   def new
     @time_slot = TimeSlot.new
+    @user = User.find(params[:user_id])
+    @appointment = Appointment.find_by_slug(params[:id])
   end
 
   # GET /time_slots/1/edit
   def edit
+    time_slot_id = params[:id]
+    @time_slot = time_slot_id(time_slot_id)
+    render :edit
   end
 
   # POST /time_slots
@@ -28,25 +33,32 @@ class TimeSlotsController < ApplicationController
       @time_slot = TimeSlot.new(time_slot_params)
       @user = User.find(params(:user_id))
       @appointment = Appointment.find_by_slug(params[:id])
+      respond_to do |format|
       if @time_slot.save
         @appointment.time_slots << @time_slot
         @current_user.time_slots << @time_slot
         @appointment.save
         @current_user.save
-        flash[:notice] = "Thank you for joining "+ @appointment.date
-
-    respond_to do |format|
-      if @time_slot.save
+        flash[:notice] = "Successful time slot at "+ @appointment.date
         format.html { redirect_to @time_slot, notice: 'Time slot was successfully created.' }
         format.json { render :show, status: :created, location: @time_slot }
+        redirect_to @appointment
+      elsif @time_slot.destroy
+        flash[:error] = "There was a problem, try again"
+        redirect_to @appointment
       else
+        flash[:error] = "You must be admin to create timeslots"
         format.html { render :new }
         format.json { render json: @time_slot.errors, status: :unprocessable_entity }
+        redirect_to new_time_slot_path
       end
     end
+
   # PATCH/PUT /time_slots/1
   # PATCH/PUT /time_slots/1.json
   def update
+    get_id.update_attributes(time_slot_params)
+    redirect_to user_path(user)
     respond_to do |format|
       if @time_slot.update(time_slot_params)
         format.html { redirect_to @time_slot, notice: 'Time slot was successfully updated.' }
@@ -62,6 +74,7 @@ class TimeSlotsController < ApplicationController
   # DELETE /time_slots/1.json
   def destroy
     @time_slot.destroy
+    redirect_to user_path
     respond_to do |format|
       format.html { redirect_to time_slots_url, notice: 'Time slot was successfully destroyed.' }
       format.json { head :no_content }
@@ -78,4 +91,3 @@ class TimeSlotsController < ApplicationController
     def time_slot_params
       params.require(:time_slot).permit(:name, :start_time)
     end
-end
