addb8a3656fcf77456e27872c516411a4fd16590
relative-rene <rene.arellano415@gmail.com>
2016-06-19 14:51:54 -0700

    controller time_slot refactored

--- app/controllers/time_slots.rb
+++ app/controllers/time_slots.rb
@@ -1,28 +1,23 @@
 class Time_slotsController < ApplicationController
+  before_action :check_admin? [:create, :edit, :update, :destroy]
+
   def index
     @time_slot = Time_slot.all
-    @user = User.find_by_id(params[:id])
     render :index
   end
 
   def show
     @time_slot = Time_slot.find_by_slug(params[:id])
-    @comments = @time_slot.comments
     render :show
   end
 
   def new
     @time_slot = Time_slot.new
-    @user = User.find_by_id(params[:id])
   end
 
   def create
     @time_slot = Time_slot.new(time_slot_params)
     @time_slot.user_id = current_user.id
-    @user = User.find_by_id(params[:id])
-    if @time_slot.photo == ""
-      @time_slot.photo = "/placeholder.jpg"
-    end
     if @time_slot.save
       flash[:notice] = "Let's party! Your time_slot has been successfully created!"
       redirect_to time_slot_path(@time_slot)
@@ -34,7 +29,6 @@ class Time_slotsController < ApplicationController
 
   def edit
     @time_slot = Time_slot.find_by_slug(params[:id])
-    @user = User.find_by_id(params[:id])
     render :edit
   end
 
@@ -56,7 +50,13 @@ class Time_slotsController < ApplicationController
 
   private
 
+  def check_admin?
+    if @current_user.admin == true
+    end
+  end
+
+
   def time_slot_params
-    params.require(:time_slot).permit(:title, :location, :time_slot_date, :time_slot_time, :description, :photo, :slug)
+    params.require(:time_slot).permit(:hour, :time)
   end
 end
