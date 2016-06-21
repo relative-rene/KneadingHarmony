781075fd672bcd49a6a8ddff1d8f28ba87ef71b4
relative-rene <rene.arellano415@gmail.com>
2016-06-21 13:36:49 -0700

    rerouted embedded timeSlots

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  root to: 'welcome#index'
+  root to: 'users#index'
     #User routes
   get "/users", to: "users#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"
@@ -16,23 +16,23 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy"
   post "/sessions", to: "sessions#create"
 
+  #Time_Slot
+  get "/time_slots", to: "time_slots#index", as: "time_slots"
+  get "/time_slots/new", to: "time_slots#new", as: "new_time_slot"
+  post "/time_slots", to: "time_slots#create", as: "create_time_slot"
+  get "/time_slots/:id", to: "time_slots#show", as: "time_slot"
+  get "/time_slots/:id/edit", to: "time_slots#edit", as: "edit_time_slot"
+  patch "/time_slots/:id", to: "time_slots#update", as: "update_time_slot"
+  delete "/time_slots/:id", to: "time_slots#destroy", as: "delete_time_slot"
 
-  #appointments
-get '/time_slots/:id/appointments/new', to: "appointments#new", as: "new_appointment"
-post '/time_slots/:id/appointments/create', to: "appointments#create", as: "create_appointment"
-get '/appointments/:id/edit', to: "appointments#edit", as: "edit_appointment"
-patch '/appointments/:id', to: "appointments#update"
-delete '/time_slots/:time_slot_id/appointments/:appointment_id', to: "appointments#destroy", as: "delete_appointment"
 
-
-#time_slots
-get "/time_slots", to: "time_slots#index", as: "time_slots"
-get "/time_slots/new", to: "time_slots#new", as: "new_time_slot"
-post "/time_slots", to: "time_slots#create", as: "create_time_slot"
-get "/time_slots/:id", to: "time_slots#show", as: "time_slot"
-get "/time_slots/:id/edit", to: "time_slots#edit", as: "edit_time_slot"
-patch "/time_slots/:id", to: "time_slots#update", as: "update_time_slot"
-delete "/time_slots/:id", to: "time_slots#destroy", as: "delete_time_slot"
+  #Appointment
+  get '/time_slots/:id/appointments/new', to: "appointments#new", as: "new_appointment"
+  post '/time_slots/:id/appointments/create', to: "appointments#create", as: "create_appointment"
+  get '/time_slots/:id/appointments/:id', to: "appointments#show", as: "appointment"
+  get '/appointments/:id/edit', to: "appointments#edit", as: "edit_appointment"
+  patch '/appointments/:id', to: "appointments#update"
+  delete '/time_slots/:time_slot_id/appointments/:appointment_id', to: "appointments#destroy", as: "delete_appointment"
 
   #About route
   get "/about", to: "about#show", as: "about"
