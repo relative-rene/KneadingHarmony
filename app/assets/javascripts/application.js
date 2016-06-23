307e5f9a7bdf65bf1f9d587016302bae19263849
relative-rene <rene.arellano415@gmail.com>
2016-06-22 18:28:46 -0700

    schema migration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,39 @@ $(document).on('keyup keypress', 'form input[type="text"]', function(e) {
     return false;
   }
 });
+
+$(document).ready(function(){
+   $('.datepicker').datepicker();
+   $(".datepicker").change(function(){
+      date_val=$(".datepicker").val();
+      if (date_val===""){
+         return;
+      }
+      $.ajax({
+         method: 'get',
+         datatype: 'json',
+         url: '/appointments/availability',
+         data: {date: date_val},
+         success: onSuccess,
+         error: onError,
+      });
+   });
+   function create_item(appointment){
+      output='<div class="radio"><label><input name="time" type="radio" value="';
+      output+=appointment;
+      output+='">';
+      output+=appointment;
+      output+='</label></div>';
+      return output;
+   }
+   function onSuccess(data, status, jqxhr) {
+      output="";
+      available_app=data.forEach(function(item){
+         output+=create_item(item);
+      });
+      $(".scrolly").html(output);
+   }
+   function onError(jqxhr, status, error) {
+      $(".scrolly").html("error");
+   }
+});
