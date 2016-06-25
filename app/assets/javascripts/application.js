d94364869f76d0b602480a55425a80635195be26
relative-rene <rene.arellano415@gmail.com>
2016-06-24 18:18:22 -0700

    timeslot partially debugged, better errors and pry gems installed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
 //= require_tree .
 
 $(document).on('keyup keypress', 'form input[type="text"]', function(e) {
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,10 +1,9 @@
 class Timeslot < ActiveRecord::Base
   belongs_to :appointment
-  has_many :users, through: :appointment
 
   extend FriendlyId
     friendly_id :user_id, use: :slugged
-    
+
   def self.confirm(params)
     @timeslot = Timeslot.find_by_id(params[:id])
     @timeslot.try(:authenticate, params[:hour])
