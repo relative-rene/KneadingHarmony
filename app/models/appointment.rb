55cbd8d235a6fd92b94d54d87c83d232fd4f7a72
relative-rene <rene.arellano415@gmail.com>
2016-06-28 23:09:35 -0700

    debugging MC

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,15 @@
 class Appointment < ActiveRecord::Base
   belongs_to :user
   belongs_to :timeslot
+  attr_accessor :date, :time
+
+  extend FriendlyId
+    friendly_id :user_id, use: :slugged
+
+
+  def self.confirm(params)
+    @appointment = Appointment.find_by_id(params[:id])
+    @appointment.try(:authenticate, params[:user_id])
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   # include validations once mvc is setup
-  # validates :email, :password, presence: true, uniqueness:true
-  # validates_confirmation_of :password
-  has_many :appointments, dependent: :destroy
-  has_many :timeslots, through: :appointments
+  # validates :email, :password_digest, presence: true, uniqueness:true
+  # validates_confirmation_of :password_digest
 
+  has_many :appointments
+  has_many :timeslots, through: :appointments
+  attr_accessor :user, :name, :email, :phone_number, :admin
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
