f6223d46d29824307027ef341517d672751c6ae8
relative-rene <rene.arellano415@gmail.com>
2016-07-11 11:56:25 -0700

    reposition jumbotron title

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,4 @@ class Appointment < ActiveRecord::Base
   belongs_to :users
   belongs_to :timeslots
 
-  accepts_nested_attributes_for :users
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :appointments
   has_many :timeslots, through: :appointments
-  accepts_nested_attributes_for :appointments
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
