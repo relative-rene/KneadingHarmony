24814579104ee50d84b402afb0eaa540dfea499f
relative-rene <rene.arellano415@gmail.com>
2016-09-27 14:31:13 -0700

    [chore] minor syntax error

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,41 +1,37 @@
 class OrderItem < ActiveRecord::Base
-  belongs_to :product
-  belongs_to :order
+    belongs_to :product
+    belongs_to :order
 
-  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validate :product_present
-  validate :order_present
+    validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    validate :product_present
+    validate :order_present
 
-  before_save :finalize
+    before_save :finalize
 
-  def unit_price
-    if persisted?
-      self[:unit_price]
-    else
-      product.price
+    def unit_price
+        if persisted?
+            self[:unit_price]
+        else
+            product.price
+        end
     end
-  end
 
-  def total_price
-    unit_price * quantity
-  end
-
-private
-  def product_present
-    if product.nil?
-      errors.add(:product, "is not valid or is not active.")
+    def total_price
+        unit_price * quantity
     end
-  end
 
-  def order_present
-    if order.nil?
-      errors.add(:order, "is not a valid order.")
+    private
+
+    def product_present
+        errors.add(:product, 'is not valid or is not active.') if product.nil?
     end
-  end
 
-  def finalize
-    self[:unit_price] = unit_price
-    self[:total_price] = quantity * self[:unit_price]
-  end
-end
+    def order_present
+        errors.add(:order, 'is not a valid order.') if order.nil?
+    end
+
+    def finalize
+        self[:unit_price] = unit_price
+        self[:total_price] = quantity * self[:unit_price]
+    end
 end
