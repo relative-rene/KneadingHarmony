73217c96512a58cc831b8c2a02004d1bef706266
relative-rene <rene.arellano415@gmail.com>
2016-09-28 14:47:48 -0700

    [chore]image upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
+        @user.profile_image = '/default_image.jpeg' if @user.profile_image == ''
         if @user.save
             flash[:notice] = 'Welcome to Kneading Harmony! Your profile has been successfully created!'
             login(@user)
@@ -62,6 +63,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :admin, :password, :phone_number, :password_confirmation, appointment_attributes: [:id, :date, :time, :reason_for_visit])
+        params.require(:user).permit(:name, :email, :profile_image, :admin, :password, :phone_number, :password_confirmation, appointment_attributes: [:id, :date, :time, :reason_for_visit])
     end
 end
--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! @user, :id, :name, :email, :password, :admin, :created_at, :updated_at
+json.extract! @user, :id, :name, :email, :password, :admin, :profile_image, :created_at, :updated_at
 json.appointments.extract! @user.appointments, :user_id, :timeslot_id, :date, :start, :duration, :reason_for_visit
