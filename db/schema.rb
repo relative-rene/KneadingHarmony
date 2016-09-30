d616b7dc6b9f26a16ea042df02b50f584df885e1
relative-rene <rene.arellano415@gmail.com>
2016-09-29 17:48:50 -0700

    [feature] subscription migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920211921) do
+ActiveRecord::Schema.define(version: 20160930004330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,12 @@ ActiveRecord::Schema.define(version: 20160920211921) do
     t.boolean  "active"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "stripe_customer_token"
   end
 
   create_table "timeslots", force: :cascade do |t|
