5c0227d43ce2a0e02e1f19ffe5457d641b86ea65
relative-rene <rene.arellano415@gmail.com>
2016-10-05 13:23:50 -0700

    Adjusting Entity relationship model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,13 +1,13 @@
 class Subscription < ActiveRecord::Base
-  belongs_to :plan
-  validates_presence_of :plan_id
+  belongs_to :product
+  validates_presence_of :product_id
   validates_presence_of :email
 
   attr_accessor :stripe_card_token
 
     def save_with_payment
         if valid?
-            customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+            customer = Stripe::Customer.create(description: email, product: product_id, card: stripe_card_token)
             self.stripe_customer_token = customer.id
             save!
         end
